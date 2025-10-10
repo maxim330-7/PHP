f1109b8564a5a1a12594ff48038835dabe5b023c
maxim330-7 <max.shep444@gmail.com>
2025-10-10 18:20:22 +0300

    Update vars.php

--- PHP/lab1/vars.php
+++ PHP/lab1/vars.php
@@ -1,31 +1,42 @@
 <?php
-$name = 'Домовой';
+/*
+ЗАДАНИЕ 1
+- Создайте переменную $name и присвойте ей значение содержащее Ваше имя, например 'Иван'(обязательно в одинарных кавычках!).
+- Создайте переменную $age и присвойте ей значение содержащее Ваш возраст, например 20.
+*/
+$name = 'Максим';
 $age = 20;
-$name_tip = gettype($name);
-$age_tip= gettype($age);
+
+// ЗАДАНИЕ 2
+// - Выведите с помощью echo фразу "Меня зовут: $name".
+// - Выведите фразу "Мне $age лет".
+// - Выведите информацию о типе переменных $name и $age.
+// - Удалите переменные $name и $age.
+// - Каждая фраза с новой строки.
 ?>
 <!DOCTYPE html>
 <html lang="ru">
-
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Переменные и вывод</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Переменные и вывод</title>
 </head>
-
 <body>
-    <h1>Переменные и вывод</h1>
-    <p>Меня зовут: <?php echo $name; ?></p>
-    <p>Мне <?php echo $age; ?> лет</p>
-    <p>Тип переменной name: <?php echo $nameType; ?></p>
-    <p>Тип переменной age: <?php echo $ageType; ?></p>
-    <?php 
-    unset($name);
-    unset($age);
-    ?>
+	<h1>Переменные и вывод</h1>
 
+	<?php
+	// Выводим данные
+	echo "Меня зовут: $name<br>";
+	echo "Мне $age лет<br>";
+	echo "Тип переменной \$name: " . gettype($name) . "<br>";
+	echo "Тип переменной \$age: " . gettype($age) . "<br>";
 
+	// Удаляем переменные
+	unset($name, $age);
+
+	echo "Переменные удалены.";
+	?>
+	
 </body>
-
 </html>
