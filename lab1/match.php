a03d9683e36bba6061b03a3ca038cd29d9a6ff12
maxim330-7 <max.shep444@gmail.com>
2025-10-24 10:37:06 +0300

    Update match.php

--- lab1/match.php
+++ lab1/match.php
@@ -1,6 +1,5 @@
 <?php
-    $a = 6;
-    $day = (int) $a;
+$day = 6;
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -9,21 +8,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Конструкция switch</title>
+    <title>Конструкция match</title>
 </head>
 
 <body>
-    <h1>Конструкция switch</h1>
+    <h1>Конструкция match</h1>
     <?php
 
-    $output = match (true) {
-        $day < 1 || $day >7 => "Неизвестный день",
-        $day > 1 && $day <6 => "Это рабочий день",
-        $day > 5 && $day <8 => "Это выходной день",
-};
+	$output = match ($day) {
+		1, 2, 3, 4, 5 => "Это рабочий день",
+		6, 7 => "Это выходной день",
+		default => "Неизвестный день",
+	};
 
-var_dump($output);
-
+	echo $output;
 	?>
 </body>
 
