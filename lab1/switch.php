78be1763ab6ea689d61cd913b2ba50ae99f1f1a0
maxim330-7 <max.shep444@gmail.com>
2025-10-24 10:34:55 +0300

    Update switch.php

--- lab1/switch.php
+++ lab1/switch.php
@@ -17,29 +17,19 @@ $max = 7;
 <body>
     <h1>Конструкция switch</h1>
     <?php
-	switch ($day){
-	    case 1:
-	        echo "Это рабочий день";
-	        break;
-	    case 2:
-	        echo "Это рабочий день";
-	        break;
-	    case 3:
-	        echo "Это рабочий день";
-	        break;
-	    case 4:
-	        echo "Это рабочий день";
-	        break;
-	    case 5:
-	        echo "Это рабочий день";
-	        break;
-	    case 6:
-	        echo "Это выходной день";
-	        break;
-	    case 7:
-	        echo "Это выходной день";
-	        break;
-	    default:
+	switch ($day) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			echo "Это рабочий день";
+			break;
+		case 6:
+		case 7:
+			echo "Это выходной день";
+			break;
+		default:
 			echo "Неизвестный день";
 			break;
 	}
