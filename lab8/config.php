c7dff1be22d49ae59c97e0aee1a63b1216d5871c
maxim330-7 <max.shep444@gmail.com>
2025-12-07 23:53:53 +0300

    Update config.php

--- lab8/config.php
+++ lab8/config.php
@@ -2,9 +2,10 @@
 declare(strict_types=1);
 
 // Настройки подключения к базе данных
-define('DB_NAME', 'f1177906_db');
-define('DB_USER', 'f1177906_db');
-define('DB_PASSWORD', 'Mgdn11238#');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8mb4');
+
 ?>
