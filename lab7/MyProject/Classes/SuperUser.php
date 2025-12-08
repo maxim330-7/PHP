a76662035ba6cdfbe2393f8bf15bb2f4a4787e36
maxim330-7 <max.shep444@gmail.com>
2025-12-08 18:06:02 +0300

    Update SuperUser.php

--- lab7/MyProject/Classes/SuperUser.php
+++ lab7/MyProject/Classes/SuperUser.php
@@ -2,15 +2,45 @@
 namespace MyProject\Classes;
 
 class SuperUser extends User {
+     /**
+     * Роль суперпользователя в системе
+     * @var string $role Роль пользователя в системе
+     * @access public
+     */
     public $role;
 
+    /**
+     * Конструктор SuperUser
+     * @param string $name Полное имя суперпользователя
+     * @param string $login Логин суперпользователя
+     * @param string $password Пароль суперпользователя
+     * @param string $role Роль суперпользователя в системе
+     * 
+     * @return void
+     * @access public
+     * 
+     * @uses User::__construct() Для инициализации базовых свойств
+     */
     public function __construct(string $name, string $login, string $password, string $role) {
         parent::__construct($name, $login, $password);
         $this->role = $role;
     }
 
-    /**
-     * Выводит информацию о суперпользователе
+   /**
+     * Переопределенный метод для отображения информации о суперпользователе
+     * 
+     * Выводит расширенную информацию о суперпользователе,
+     * включая его роль. 
+     * @return void
+     * @access public
+     * @override
+     * 
+     * @see User::showInfo() 
+     * 
+     * @example
+     * $admin = new SuperUser("Админ", "admin", "pass123", "Главный администратор");
+     * $admin->showInfo();
+     * // Выведет информацию с указанием роли пользователя
      */
     public function showInfo(): void {
         echo "<div style='border: 3px solid #f00; padding: 10px; margin: 10px; background: #FFD700;'>";
@@ -22,4 +52,5 @@ class SuperUser extends User {
         echo "</div>";
     }
 }
+
 ?>
